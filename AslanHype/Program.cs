423ecfc485409d78ce0f8518d869692bf00dab5b
werdbrian <werdbrian@gmail.com>
2015-11-22 13:43:11 -0800

    Update Program.cs

--- AslanHype/Program.cs
+++ AslanHype/Program.cs
@@ -72,7 +72,7 @@ namespace AslanHype
                 {
                     if (Config.Item("track." + hero.ChampionName).GetValue<bool>())
                     {
-                        if (hero.Distance(ObjectManager.Player.Position) <= 8000 && hero.Distance(ObjectManager.Player.Position) > 1000 )
+                        if (hero.Distance(ObjectManager.Player.Position) <= 8000 && hero.Distance(ObjectManager.Player.Position) > 1500 )
                         {
                             Drawing.DrawLine(Drawing.WorldToScreen(ObjectManager.Player.Position), Drawing.WorldToScreen(hero.Position), 1, Color.Green);
                             var currentScreenPlayer = Drawing.WorldToScreen(hero.Position);
