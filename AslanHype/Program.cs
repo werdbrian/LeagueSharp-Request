82d9d26a51c93aedf59504f81267e20b715c927a
werdbrian <werdbrian@gmail.com>
2015-11-22 13:03:16 -0800

    Update Program.cs

--- AslanHype/Program.cs
+++ AslanHype/Program.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
-using System.Drawing;
+using System.Globalization;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using LeagueSharp;
 using LeagueSharp.Common;
 using SharpDX;
-
+using Color = System.Drawing.Color;
 
 namespace AslanHype
 {
