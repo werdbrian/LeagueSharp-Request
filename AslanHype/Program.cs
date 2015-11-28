d49086f90e919be3dfb5d9f0c89f5a6e3a1868b1
werdbrian <werdbrian@gmail.com>
2015-11-28 13:51:04 -0800

    Update Program.cs

--- AslanHype/Program.cs
+++ AslanHype/Program.cs
@@ -80,7 +80,7 @@ namespace AslanHype
                             var currentScreenEnemy = Drawing.WorldToScreen(hero.Position);
                             var _line = new Vector2(currentScreenChamp.X-currentScreenEnemy.X,currentScreenChamp.Y-currentScreenEnemy.Y);
                            // var direction = ObjectManager.Player.Direction.To2D().Perpendicular();
-                            var direction = _line;
+                            var direction = _line.Perpendicular();
                             var playerPosition = ObjectManager.Player.Position.To2D();
                             var conePoint1 = playerPosition + 800 * direction.Rotated(currentAngel1);
                             var conePoint2 = playerPosition + 800 * direction.Rotated(currentAngel2);
